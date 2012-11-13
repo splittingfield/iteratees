5417417a9e32cb33e29bdbce919c01d556f89e19
Marc Millstone <marc.millstone@socrata.com>
2012-11-12 16:09:40 -0800

    Added ZipEnumeratee

--- src/main/scala/com/socrata/iteratee/Main.scala
+++ src/main/scala/com/socrata/iteratee/Main.scala
@@ -20,15 +20,13 @@ class WordEnumeratee[T](wordFragment:Option[String], iteratee:Iteratee[String,T]
       case Some(string) => string + str
       case None => str
     }
-
-    val lastStringIsFullWord = if (fullString != Nil && fullString(fullString.length-1) == ' ') true else false
-    val possibleFullWords = fullString.split(" +").toList
+    val possibleFullWords = fullString.split(" +").toList.filter(_.nonEmpty)
     @tailrec
     def loop(words:List[String], newIter:Iteratee[String,T]):Either[WordEnumeratee[T],T] = {
       words match {
         case Nil => Left(new WordEnumeratee[T](None,newIter))
         case hd::Nil =>
-          if (lastStringIsFullWord)
+          if (fullString.endsWith(" "))
             newIter.process(hd) match {
               case Right(done) => Right(done)
               case Left(notDone) => Left(new WordEnumeratee[T](None, notDone))
@@ -51,28 +49,50 @@ class WordCountIteratee(counts:Map[String,Int]) extends Iteratee[String,Map[Stri
 
 }
 
+class WordCountIteratee2(counts:Map[String,Int]) extends Iteratee[String,Map[String,Int]] {
+  override def endOfInput() = counts
+  override def process(word:String) = Left(new WordCountIteratee2(counts.updated(word.toLowerCase,counts.getOrElse(word.toLowerCase,0)+2)))
+
+}
+
+
+class ZipEnumeratee[T,O1,O2](it1:Iteratee[T,O1], it2:Iteratee[T,O2]) extends Iteratee[T,(O1,O2)] {
+  def endOfInput = (it1.endOfInput(), it2.endOfInput())
+  def process(input:T) = {
+    it1.process(input) match {
+      case Right(result) => it2.map((result,_)).process(input)
+      case Left(newA) =>
+        it2.process(input) match {
+          case Right(result) => Left(newA.map((_,result)))
+          case Left(newB) => Left(new ZipEnumeratee(newA,newB))
+      }
+    }
+  }
+}
 
 object Main {
   def main(args:Array[String]) {
 
     val wci = new WordCountIteratee(Map[String,Int]())
+    val wc2 = new WordCountIteratee2(Map[String,Int]())
     val wce = new WordEnumeratee(None,wci)
+    val zip = new ZipEnumeratee(wci,wc2)
+    print(">> ")
+    val ln = Console.readLine()
+//   val a = ln.split('!').foldLeft(zip:Iteratee[String,(Map[String,Int], Map[String,Int])]){ case (enum,word) =>
+    //      enum.process(word) match {
+    //        case Left(notDone) => notDone
+    //        case Right(done) => enum
+    //      }
 
-    @tailrec
-    def getInput[T](enum:WordEnumeratee[T]):WordEnumeratee[T] = {
-      print(">> ");
-      val ln = Console.readLine()
-      if (ln == "")
-        enum
-      else {
-        enum.process(ln) match {
-          case Left(notDone) =>  getInput(notDone)
+    val a = ln.split('!').foldLeft(wce){ case (enum,word) =>
+        enum.process(word) match {
+          case Left(notDone) => notDone
           case Right(done) => enum
         }
-      }
     }
 
-    val a = getInput(wce)
+
     println(a.endOfInput())
 
 
